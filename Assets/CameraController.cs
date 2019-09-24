111903fa9444ce1e09ac62553a28ff26b71cb824
John <jmpwonson@gmail.com>
2019-09-24 20:19:22 +1000

    Prototype update
    Updated the most recent version with an added pressure plate

---
+++ Assets/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Ball;
+    public float xOffset = 0, yOffset = 5, zOffset = -5;
+
+    void Update()
+    {
+        transform.position = Ball.transform.position + new Vector3(xOffset, yOffset, zOffset);
+        transform.LookAt(Ball.transform.position);
+    }
+}
